73b48589cc815fa5aa46715631a5318447f99378
ktran05 <143561220+ktran05@users.noreply.github.com>
2025-03-18 14:58:32 -0500

    Update MyNodeBuilder.java

--- MyClassProject/src/myclassproject/mystorygraph/MyNodeBuilder.java
+++ MyClassProject/src/myclassproject/mystorygraph/MyNodeBuilder.java
@@ -22,6 +22,23 @@ public class MyNodeBuilder extends NodeBuilder {
 		//Example:
 		//var root = get(NodeLabels.root.toString());
 		//root.add(new CreateAll(List.of(cottage, town, sword)));
-		int test = 0;
+		//int test = 0;
+		var root = get(NodeLabels.root.toString());
+		root.add(new CreateAll(List.of("tavern", "dungeon", "camp", "hallway", "blue potion", "green potion", "chicken leg", "red potion", "jewel key")));
+		root.add(new CreateCharacterSequence(List.of("peasant", "knight", "warlock")));
+		
+		root.add(new SetPosition("peasant", "camp"));
+		root.add(new SetPosition("knight", "hallway"));
+		root.add(new SetPosition("chicken leg", "hallway"));
+		root.add(new SetPosition("sword", "hallway"));
+		root.add(new SetPosition("blue potion", "tavern"));
+		root.add(new SetPosition("green potion", "tavern"));
+		root.add(new SetPosition("red potion", "tavern"));
+		root.add(new SetPosition("jewel key", "tavern"));
+		root.add(new SetPosition("warlock", "dungeon"));
+		
+		root.add(new SetCameraFocus("peasant"));
+		
+		root.add(new ShowMenu());
 	}
 }
